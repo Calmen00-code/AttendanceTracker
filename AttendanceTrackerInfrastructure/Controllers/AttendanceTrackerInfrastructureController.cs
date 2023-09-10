3838195de61ab92af28f12a53f7cf58b2896de1c
calmen <calmenchia@gmail.com>
2023-09-10 20:23:47 +0800

    API to add admin

--- AttendanceTrackerInfrastructure/Controllers/AttendanceTrackerInfrastructureController.cs
+++ AttendanceTrackerInfrastructure/Controllers/AttendanceTrackerInfrastructureController.cs
@@ -1,7 +1,9 @@
 ﻿using APIClasses;
+using AttendanceTrackerInfrastructure.Migrations;
 using AttendanceTrackerInfrastructure.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using System.Data;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,11 +14,14 @@ namespace AttendanceTrackerInfrastructure.Controllers
     [ApiController]
     public class AttendanceTrackerInfrastructureController : ControllerBase
     {
+        // private readonly AttendanceTrackerDbContext _dbContext;
         public readonly IConfiguration _configuration;
 
+        // public AttendanceTrackerInfrastructureController(AttendanceTrackerDbContext dbContext)
         public AttendanceTrackerInfrastructureController(IConfiguration configuration)
         {
             _configuration = configuration;
+            // _dbContext = dbContext;
         }
 
         [HttpGet]
@@ -25,6 +30,7 @@ namespace AttendanceTrackerInfrastructure.Controllers
         {
             SqlConnection conn;
             SqlDataAdapter adapter;
+            DataTable dt;
             try
             {
                 // connecting to db
@@ -34,14 +40,16 @@ namespace AttendanceTrackerInfrastructure.Controllers
 
                 // building sql query
                 adapter = new SqlDataAdapter("SELECT * FROM Admins", conn);
+
+                dt = new DataTable();
+                adapter.Fill(dt);
+                conn.Close();
             }
             catch (Exception) 
             {
                 return StatusCode(500, "Failed to connect to database");
             }
 
-            DataTable dt = new DataTable();
-            adapter.Fill(dt);
 
             List<AdminAPI> admins = new List<AdminAPI>();
 
@@ -64,6 +72,70 @@ namespace AttendanceTrackerInfrastructure.Controllers
             else
             {
                 return StatusCode(404, "No data has been found");
+            }
+        }
+
+        /*
+
+        [HttpGet]
+        [Route("get-staffs")]
+        public IActionResult GetStaffs()
+        {
+            List<StaffAPI> staffs = new List<StaffAPI>();
+            try
+            {
+                _dbContext.Database.OpenConnection();
+                List<Staff> staffsDb = (from staff in _dbContext.staffs
+                                        select staff).ToList();
+
+                // we are returning List<StaffAPI> here
+                // that is why we need a static cast the List<Staff> staffsDb from the database
+                staffs = staffsDb.Cast<StaffAPI>().ToList();
+
+                return Ok(staffs);
+            }
+            catch (Exception) 
+            {
+                return StatusCode(500, "Unable to connect to the database");
+            }
+        }
+        */
+
+        [HttpPost]
+        [Route("add-admin")]
+        public IActionResult AddAdmin([FromBody] AdminAPI admin)
+        {
+            SqlConnection conn = new SqlConnection(_configuration
+                .GetConnectionString("AttendanceTracker")
+                .ToString());
+
+            // building sql query
+            string sqlScript = "INSERT INTO Admins (Name, Password) VALUES (@Name, @Password);";
+            SqlCommand sqlCommand = new SqlCommand(sqlScript, conn);
+            sqlCommand.Parameters.AddWithValue("@Name", admin.Name);
+            sqlCommand.Parameters.AddWithValue("@Password", admin.Password);
+
+            try
+            {
+                conn.Open();
+                int rowsAffected = sqlCommand.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    return StatusCode(200, "Admin successfully registered");
+                }
+                else
+                {
+                    return StatusCode(400, "Internal server error");
+                }
+            }
+            catch (SqlException e)
+            {
+                System.Diagnostics.Debug.WriteLine("Error: " + e.Message);
+                return StatusCode(500, "Internal server error");
+            }
+            finally
+            {
+                conn.Close();
             }
         }
     }
