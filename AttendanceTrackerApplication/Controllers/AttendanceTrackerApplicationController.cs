3a8cf1a9eb0d75f731d9790eeaa4062439740f83
calmen <calmenchia@gmail.com>
2023-09-16 14:18:32 +0800

    Application layer to call data access layer in registering new staff, admin, and workday record

--- AttendanceTrackerApplication/Controllers/AttendanceTrackerApplicationController.cs
+++ AttendanceTrackerApplication/Controllers/AttendanceTrackerApplicationController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using APIClasses;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace AttendanceTrackerApplication.Controllers
 {
@@ -18,14 +23,99 @@ namespace AttendanceTrackerApplication.Controllers
 
         [HttpGet]
         [Route("get-admins")]
-        public async IActionResult GetAdmins()
+        public async Task<IActionResult> GetAdmins()
         {
-            string route = _apiurl + "/get-admins";
+            string route = _apiurl + "get-admins";
             var response = await _httpClient.GetAsync(route);
 
-            if ((int)(response.StatusCode) == )
+            if ((int)(response.StatusCode) == HttpResponseStatus.OK)
             {
+                string responseContent = await response.Content.ReadAsStringAsync();
+                return StatusCode(HttpResponseStatus.OK, responseContent);
+            }
+            else
+            {
+                string errorContent = await response.Content.ReadAsStringAsync();
+                return StatusCode(HttpResponseStatus.NOT_FOUND, errorContent);
+            }
+        }
 
+        [HttpPost]
+        [Route("add-admin")]
+        public async Task<IActionResult> AddAdmin([FromBody] AdminAPI admin) 
+        {
+            // Serializing individual data of admin and used it to create
+            // a json object representation for it
+            var adminJson = JsonContent.Create(admin);
+
+            string route = _apiurl + "add-admin";
+            var response = await _httpClient.PostAsync(route, adminJson);
+
+            if ((int)(response.StatusCode) == HttpResponseStatus.CREATED)
+            {
+                /*
+                var adminContent = await response.Content.ReadAsStreamAsync();
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                var result = await JsonSerializer.DeserializeAsync<AdminAPI>(adminContent, options);
+                */
+
+                return StatusCode(HttpResponseStatus.CREATED);
+            }
+            else
+            {
+                string errorContent = await response.Content.ReadAsStringAsync();
+                return StatusCode(HttpResponseStatus.NOT_FOUND, errorContent);
+            }
+        }
+
+        [HttpPost]
+        [Route("add-staff")]
+        public async Task<IActionResult> AddStaff([FromBody] StaffAPI staff)
+        {
+            var staffJson = JsonContent.Create(staff);
+
+            string route = _apiurl + "add-staff";
+            var response = await _httpClient.PostAsync(route, staffJson);
+
+            if ((int)(response.StatusCode) == HttpResponseStatus.CREATED)
+            {
+                // var staffContent = await response.Content.ReadAsStreamAsync();
+                // var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                // var result = await JsonSerializer.DeserializeAsync<StaffAPI>(staffContent, options);
+
+                // return StatusCode(HttpResponseStatus.CREATED, result);
+                return StatusCode(HttpResponseStatus.CREATED);
+            }
+            else
+            {
+                string errorContent = await response.Content.ReadAsStringAsync();
+                return StatusCode(HttpResponseStatus.NOT_FOUND, errorContent);
+            }
+        }
+
+        [HttpPost]
+        [Route("add-workday-record")]
+        public async Task<IActionResult> AddWorkdayRecord([FromBody] WorkdayRecordAPI workdayRecord)
+        {
+            var workdayRecordJson = JsonContent.Create(workdayRecord);
+
+            string route = _apiurl + "add-workday-record";
+            var response = await _httpClient.PostAsync(route, workdayRecordJson);
+
+            if ((int)(response.StatusCode) == HttpResponseStatus.ACCEPTED)
+            {
+                /*
+                var workdayContent = await response.Content.ReadAsStreamAsync();
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                var result = await JsonSerializer.DeserializeAsync<WorkdayRecordAPI>(workdayContent, options);
+                */
+
+                return StatusCode(HttpResponseStatus.ACCEPTED);
+            }
+            else
+            {
+                string errorContent = await response.Content.ReadAsStringAsync();
+                return StatusCode(HttpResponseStatus.NOT_FOUND, errorContent);
             }
         }
     }
