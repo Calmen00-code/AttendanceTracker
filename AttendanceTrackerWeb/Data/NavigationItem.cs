b75cb497601c4e94b0241be535ea9787ae6492b0
Calmen <calmenchia@gmail.com>
2023-09-24 21:36:01 +0800

    Added new user registartion page in admin menu

---
+++ AttendanceTrackerWeb/Data/NavigationItem.cs
@@ -0,0 +1,9 @@
+﻿namespace AttendanceTrackerWeb.Data
+{
+    public class NavigationItem
+    {
+        public string Url { get; set; }
+        public string IconText { get; set; }
+        public string Label { get; set; }
+    }
+}
