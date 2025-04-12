22eef50a4ee4df67ff7528b7d15dff2199b2ed43
Calmen <calmenchia@gmail.com>
2025-04-12 23:58:22 +0800

    Setup initial database and also added admin table

---
+++ AttendanceTracker.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AttendanceTracker.Models;
+
+namespace AttendanceTracker.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Admin> Admins { get; set; }
+    }
+}
---
+++ AttendanceTracker.Models/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttendanceTracker.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
